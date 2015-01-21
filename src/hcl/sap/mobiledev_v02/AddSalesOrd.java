e53261310c1bb7d454f20a7dc1be86a445171244
yukilynn <sherlynn.yuki@gmail.com>
2015-01-21 15:09:27 +0800

    Added Business Partner List for Add Sales Order
    - Create new Adapter classes for displaying Business Partner List screen
    and Add Sales Order screen.
    - Create Business Partner List class
    - Create dummy data in project asset

--- src/hcl/sap/mobiledev_v02/AddSalesOrd.java
+++ src/hcl/sap/mobiledev_v02/AddSalesOrd.java
@@ -1,5 +1,7 @@
 package hcl.sap.mobiledev_v02;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
@@ -8,40 +10,43 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class AddSalesOrd extends ActionBarActivity {
-	private ListView listView;
+public class AddSalesOrd extends ListActivity {
+//	private ListView listView;
 
-	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.add_sls_ord);
-
-		listView = (ListView) findViewById(R.id.list);
-
-		final ListViewItem[] items = new ListViewItem[40];
-
-		for (int i = 0; i < items.length; i++) {
-			if (i == 1) {
-				items[i] = new ListViewItem("List" + i,
-						SalesOrdAdapter.TYPE_LIST);
-			} else if (i == 9) {
-				items[i] = new ListViewItem("No List" + i,
-						SalesOrdAdapter.TYPE_NONE);
-			}
-		}
-		SalesOrdAdapter soAdapter = new SalesOrdAdapter(this,
-				R.id.tvBizPartner, items);
-		listView.setAdapter(soAdapter);
-		listView.setOnItemClickListener(new OnItemClickListener() {
+		
+		final String[] values = new String[] {
+				"Doc. Series", "Business Partner", "Contact Person", "Currency",
+				"Sales Employee", "Posting Date", "Delivery Date", "Document Date",
+				"Remarks", "Ship To", "Bill To", "Total Before Discount", "Discount%",
+				"Discount", "Tax", "Total"
+		};
+		
+		SalesOrdAdapter soAdapter = new SalesOrdAdapter(this, values);
+		setListAdapter(soAdapter);
+		getListView().setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
 				// TODO Auto-generated method stub
-				Toast.makeText(getBaseContext(), items[position].getText(),
-						Toast.LENGTH_SHORT).show();
+				Intent intent = new Intent(AddSalesOrd.this, BizPartList.class);
+				startActivity(intent);
 			}
+			
 		});
+//		listView.setAdapter(soAdapter);
+//		listView.setOnItemClickListener(new OnItemClickListener() {
+//
+//			@Override
+//			public void onItemClick(AdapterView<?> parent, View view,
+//					int position, long id) {
+//				// TODO Auto-generated method stub
+//				Toast.makeText(getBaseContext(), values[position],
+//						Toast.LENGTH_SHORT).show();
+//			}
+//		});
 	}
 }
--- src/hcl/sap/mobiledev_v02/SalesOrdAdapter.java
+++ src/hcl/sap/mobiledev_v02/SalesOrdAdapter.java
@@ -5,59 +5,44 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class SalesOrdAdapter extends ArrayAdapter {
+public class SalesOrdAdapter extends ArrayAdapter<String> {
+	private final Context context;
+	private final String[] values;
+	
 	public static final int TYPE_LIST = 0;
 	public static final int TYPE_NONE = 1;
+	
+//	public ListViewItem[] objects;
 
-	public ListViewItem[] objects;
-
-	@Override
-	public int getViewTypeCount() {
-		// TODO Auto-generated method stub
-		return 2;
-	}
-
-	@Override
-	public int getItemViewType(int position) {
-		// TODO Auto-generated method stub
-		return objects[position].getType();
-	}
-
-	@SuppressWarnings("unchecked")
-	public SalesOrdAdapter(Context context, int resource, Object[] objects) {
-		super(context, resource, objects);
+	public SalesOrdAdapter(Context context, String[] values) {
+		super(context, R.layout.list_select, values);
 		// TODO Auto-generated constructor stub
-		this.objects = (ListViewItem[]) objects;
+		this.context = context;
+		this.values = values;
 	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		// TODO Auto-generated method stub
-		ViewHolder viewHolder = null;
-		ListViewItem listViewItem = objects[position];
-		int listViewItemType = getItemViewType(position);
-
-		if (convertView == null) {
-			if (listViewItemType == TYPE_LIST) {
-				convertView = LayoutInflater.from(getContext()).inflate(
-						R.layout.list_select, null);
-			} else if (listViewItemType == TYPE_NONE) {
-				convertView = LayoutInflater.from(getContext()).inflate(
-						R.layout.list_view, null);
-			}
-
-			TextView textView = (TextView) convertView
-					.findViewById(R.id.tvBizPartner);
-			viewHolder = new ViewHolder(textView);
-			convertView.setTag(viewHolder);
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View viewHolder = inflater.inflate(R.layout.list_select, parent, false);
+		TextView bizPart_lbl = (TextView) viewHolder.findViewById(R.id.tvBizPartner);
+		TextView bizPart_name = (TextView) viewHolder.findViewById(R.id.tvBizPartnerName);
+		ImageView arrow = (ImageView) viewHolder.findViewById(R.id.ivArrow);
+		bizPart_lbl.setText(values[position]);
+		String s = values[position];
+		if (s.startsWith("Business Partner") || s.startsWith("Sales Employee") || s.startsWith("Posting Date")
+				|| s.startsWith("Delivery Date") || s.startsWith("Document Date") || s.startsWith("Remarks")
+				|| s.startsWith("Ship To") || s.startsWith("Bill To")) {
+			arrow.setImageResource(R.drawable.arrow);
 		} else {
-			viewHolder = (ViewHolder) convertView.getTag();
+			arrow.setImageResource(android.R.color.transparent);
 		}
-		viewHolder.getText().setText(listViewItem.getText());
-
-		return convertView;
+		
+		return viewHolder;
 	}
 
 }
